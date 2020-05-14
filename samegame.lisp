322cb8115c8f7cdb7fd5535d9f4bb3e759d6d92d
João Palet <joao.palet@outlook.com>
2020-05-14 18:48:48 +0100

    para pull

--- samegame.lisp
+++ samegame.lisp
@@ -12,4 +12,23 @@
     (write strategy)
 )
 
+;;; recebe uma posicao e um tabuleiro e devolve as posições adjacentes
+;;; que tem a mesma cor
+(defun check-adjacents (i j board)
+    (let ((block nil))
+        ))
+
+;;; recebe uma posicao e um tabuleiro e devolve true se a posicao acima for
+;;; da mesma cor
+(defun check-up (i j board)
+    (if (/= i 0)
+        (if (eql (which-color i j board) (which-color (- i 1) j board))
+            T)
+
+;;; recebe duas cor
+(defun check-left (i j board)
+    (if (/= j 0)
+        (if (eql (which-color i j board) (which-color i (- j 1) board))
+            T)
+
 ;; (resolve-same-game problem_1 strategy_1)
