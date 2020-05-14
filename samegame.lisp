4a402888fbd069e37fdc3fc5b4f3b7f3e89ef0a8
João Palet <joao.palet@outlook.com>
2020-05-15 00:18:10 +0100

    added leader function

--- samegame.lisp
+++ samegame.lisp
@@ -1,18 +1,47 @@
-;;;; xxxx
+;;;; Projeto de Procura e Planeamento 2019/2020
+;;;;
+;;;; Joao Palet  - 86447
+;;;; Simao Nunes - 86512
+
+
 (in-package :user)
 
 
-;;;; compile and load
+;;; compile and load
 (compile-file "procura.lisp")
 (load "setup")
 (load "procura")
 
 
-;;;; main function
+;;;; estrutura que representa uma coordenada do tabuleiro
+(defstruct point
+  i
+  j
+)
+
+
+;;; main function
 (defun resolve-same-game (problem strategy)
     (write problem)
     (write strategy)
 )
+
+
+;;; recebe uma jogada e um tabuleiro e devolve o tabuleiro resultante
+(defun apply-play (i j board)
+    )
+
+
+;;; recebe uma grupo de pecas e calcula o seu representante
+(defun leader (pieces)
+    (car (sort pieces 'compare-points)))
+
+
+;;; compara dois pontos
+(defun compare-points(p1 p2)
+    (if (<= (point-i p1) (point-i p2))
+        (if (<= (point-j p1) (point-j p2))
+            T)))
 
 
 ;;; recebe uma posicao e um tabuleiro e devolve as posições adjacentes
@@ -25,7 +54,7 @@
 ;;; recebe uma posicao e um tabuleiro e devolve true se a posicao acima for
 ;;; da mesma cor
 (defun check-up (i j board)
-    (if (/= i 0)
+    (if (> i 0)
         (if (eql (which-color i j board) (which-color (- i 1) j board))
             T)))
 
@@ -33,7 +62,7 @@
 ;;; recebe uma posicao e um tabuleiro e devolve true se a posicao a esquerda
 ;;; for da mesma cor
 (defun check-left (i j board)
-    (if (/= j 0)
+    (if (> j 0)
         (if (eql (which-color i j board) (which-color i (- j 1) board))
             T)))
 
@@ -49,7 +78,7 @@
 ;;; recebe uma posicao e um tabuleiro e devolve true se a posicao a direita
 ;;; for da mesma cor
 (defun check-right (i j board)
-    (if (< i (list-length (car board)))
+    (if (< j (list-length (car board)))
         (if (eql (which-color i j board) (which-color i (+ j 1) board))
             T)))
 
