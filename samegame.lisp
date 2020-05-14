033d9f97b391490b5c6e301172bc83896f0554cf
SimaoNunes <simaspedro1@gmail.com>
2020-05-14 21:39:30 +0100

    vou jantar

--- samegame.lisp
+++ samegame.lisp
@@ -59,36 +59,63 @@
     (nth j row)
 )
 
+(defun point-in-list (p points) 
+    (loop for point in points
+        ;;; i equals
+        (setf iFlag (equal (point-i point) (point-i p)))
+        ;;; j equals
+        (setf jFlag (equal (point-j point) (point-j p)))
+        (if (and iFlag jFlag) T)
+    )
+    nil
+)
+
+;; (write (point-in-list (make-point :i i :j j) ((make-point :i i :j j))))
+
 ;;; recebe uma posicao e um tabuleiro e devolve as posições adjacentes
 ;;; que tem a mesma cor
 (defun check-group (i j board)
-    ;;;; array inicial
+
+
+    (setf queue (list (make-point :i i :j j)))
+    (setf visited (list (make-point :i i :j j)))
     (setf group (list (make-point :i i :j j)))
-    (if (check-up i j board)
-        (progn 
-            (push (make-point :i (- i 1) :j j) group)
-            (write "up")
+
+
+    (loop while (> (list-length queue) 0)
+
+        (setf ball (pop queue))
+
+
+
+
+        (if (check-up i j board)
+            (progn 
+                (push (make-point :i (- i 1) :j j) group)
+                (write "up")
+            )
         )
-    )
-    (if (check-down i j board)
-        (progn 
-            (push (make-point :i (+ i 1) :j j) group)
-            (write "down")
+        (if (check-down i j board)
+            (progn 
+                (push (make-point :i (+ i 1) :j j) group)
+                (write "down")
+            )
         )
-    )
-    (if (check-left i j board)
-        (progn 
-            (push (make-point :i i :j (- j 1)) group)
-            (write "left")
+        (if (check-left i j board)
+            (progn 
+                (push (make-point :i i :j (- j 1)) group)
+                (write "left")
+            )
         )
-    )
-    (if (check-right i j board)
-        (progn 
-            (push (make-point :i i :j (+ j 1)) group)
-            (write "right")
+        (if (check-right i j board)
+            (progn 
+                (push (make-point :i i :j (+ j 1)) group)
+                (write "right")
+            )
         )
+        group    
     )
-    group
+
 )
 
 
