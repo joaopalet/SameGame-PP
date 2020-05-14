060bc452e2b68ecc7584b771d021d2964b25c815
SimaoNunes <simaspedro1@gmail.com>
2020-05-14 18:45:17 +0100

    which color and gitignore

--- samegame.lisp
+++ samegame.lisp
@@ -12,4 +12,13 @@
     (write strategy)
 )
 
+;;;; funcao que devolve a cor de uma determinada posicao
+(defun which-color (row column board)
+    ;; linha correspondente
+    (setf row (nth row board))
+    ;; cor 
+    (nth column row)
+)
+
 ;; (resolve-same-game problem_1 strategy_1)
+;; (write (which-color 3 9 problem_1))
